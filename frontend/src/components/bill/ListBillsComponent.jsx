9891de30f1667731b07fb441e46eb12e6f53a9b4
vonhu <vonhu.alien@gmail.com>
2022-12-02 11:58:36 +0700

    fix bug header

--- frontend/src/components/bill/ListBillsComponent.jsx
+++ frontend/src/components/bill/ListBillsComponent.jsx
@@ -13,7 +13,6 @@ export default function ListBillsComponent() {
     useEffect(() => {
         BillService.getAllBills()
             .then(res => {
-                console.log(res.data)
                 setBills(res.data)
                 setLoginStatus(true)
             })
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -15,59 +15,87 @@ function Header(props) {
    }, [])
 
     return (
-        <header className="header">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-xl-3 col-lg-2">
-                        <div className="header__logo">
-                            <Link to="/"><img src="img/logo.png" alt="" /></Link>
-                        </div>
-                    </div>
-                    <div className="col-xl-6 col-lg-7">
-                        <nav className="header__menu">
-                            <ul>
-                                <li className="active"><a href="/">Home</a></li>
-                                <li><a href={`/product/gender/false`}>Women’s</a></li>
-                                <li><a href={`/product/gender/true`}>Men’s</a></li>
-                                <li><a >Brands</a>
-                                    <ul className="dropdown">
-                                        {
-                                            brands.map(brand =>
-                                                <li key={brand.id}><a href={`/product/brand/${brand.name}`}>{brand.name}</a></li>
-                                            )
-                                        }
-
-                                    </ul>
-                                </li>
-                                <li><Link to="/blog.html">Blog</Link></li>
-                                <li><Link to="/logout">{props.status ? 'Logout' : ''}</Link></li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className="col-lg-3">
-                        <div className="header__right">
-                            <div className="header__right__auth">
-                                <Link to="/login">{!props.status ? 'Login' : ' '} </Link>
-                                <Link to="/sigin">Register</Link>
-                            </div>
-                            <ul className="header__right__widget">
-                                <li><span className="icon_search search-switch"></span></li>
-                                <li><Link to="/bill"><span className="icon_heart_alt"></span>
-                                    <div className="tip"></div>
-                                </Link></li>
-                                <li><Link to="/cart"><span className="icon_bag_alt"></span>
-                                    <div className="tip"></div>
-                                </Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className="canvas__open">
-                    <i className="fa fa-bars"></i>
-                </div>
+      <header className="header">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xl-3 col-lg-2">
+              <div className="header__logo">
+                <Link to="/">
+                  <img src="img/logo.png" alt="" />
+                </Link>
+              </div>
             </div>
-        </header>
-    )
+            <div className="col-xl-6 col-lg-7">
+              <nav className="header__menu">
+                <ul>
+                  <li className="active">
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a href={`/product/gender/false`}>Women’s</a>
+                  </li>
+                  <li>
+                    <a href={`/product/gender/true`}>Men’s</a>
+                  </li>
+                  <li>
+                    <a>Brands</a>
+                    <ul className="dropdown">
+                      {brands.map((brand) => (
+                        <li key={brand.id}>
+                          <a href={`/product/brand/${brand.name}`}>
+                            {brand.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                  <li>
+                    {/* <Link to="/blog.html">Blog</Link> */}
+                  </li>
+                  <li>
+                    <Link to="/logout">{props.status ? "Logout" : ""}</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="col-lg-3">
+              <div className="header__right">
+                <div className="header__right__auth">
+                  {!props.status ? (
+                    <>
+                      <Link to="/login">Login</Link>
+                      <Link to="/sigin">Register</Link>
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <ul className="header__right__widget">
+                  <li>
+                    <span className="icon_search search-switch"></span>
+                  </li>
+                  <li>
+                    <Link to="/bill">
+                      <span className="icon_heart_alt"></span>
+                      <div className="tip"></div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/cart">
+                      <span className="icon_bag_alt"></span>
+                      <div className="tip"></div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="canvas__open">
+            <i className="fa fa-bars"></i>
+          </div>
+        </div>
+      </header>
+    );
 }
 
 export default Header;
